1fb5273b2627a50eba77d4ef8c8c2d6ca618f199
dhruv304c2 <62604491+dhruv304c2@users.noreply.github.com>
2022-11-15 12:18:21 +0530

    init

---
+++ Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,16 @@
+﻿using Types;
+
+namespace Cards
+{
+    public struct Card
+    {
+        public Numbers Numbers;
+        public Suits Suit;
+
+        public Card(Numbers numbers, Suits suit)
+        {
+            Numbers = numbers;
+            Suit = suit;
+        }
+    }
+}
---
+++ Assets/Scripts/Types/Numbers.cs
@@ -0,0 +1,19 @@
+﻿namespace Types
+{
+    public enum Numbers
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+}
