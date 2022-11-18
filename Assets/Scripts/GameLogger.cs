4c9958974f9f54248e92b159b4d8341abef5fd76
dhruv304c2 <62604491+dhruv304c2@users.noreply.github.com>
2022-11-18 15:42:11 +0530

    pushing game logger script

---
+++ Assets/Scripts/GameLogger.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Game
+{
+    public class GameLogger : MonoBehaviour
+    {
+        public TextMeshProUGUI text;
+        public Color normalLogColor;
+        public Color errorLogColor;
+
+        public static void Log(string message)
+        {
+            Instance.text.text = message;
+            Instance.text.color = Instance.normalLogColor;
+        }
+        
+        public static void LogError(string message)
+        {
+            Instance.text.text = message;
+            Instance.text.color = Instance.errorLogColor;
+        }
+
+        public static GameLogger Instance => FindObjectOfType<GameLogger>();
+    }
+}
